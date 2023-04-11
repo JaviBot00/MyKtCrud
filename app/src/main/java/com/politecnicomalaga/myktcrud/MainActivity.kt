87c779a4f04f73a7c1fd9cda92642da8c336ad5a
JaviBot00 <javibot00@gmail.com>
2023-04-11 15:30:34 +0200

    Empezando Kotlin

--- app/src/main/java/com/politecnicomalaga/myktcrud/MainActivity.kt
+++ app/src/main/java/com/politecnicomalaga/myktcrud/MainActivity.kt
@@ -1,17 +1,80 @@
 package com.politecnicomalaga.myktcrud
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.splashscreen.SplashScreen
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputLayout
+
 
 class MainActivity : AppCompatActivity() {
 
-    private val TextInputEditText textInputUser
+    val REGISTER_REQUEST = 1
+    val RECYCLER_REQUEST = 2
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-SplashScreen.installSplashScreen()
+        val splashScreen = installSplashScreen()
         setContentView(R.layout.activity_main)
+
+        val textInputUser: TextInputLayout = findViewById(R.id.txtFldUser)
+        val textInputPassword: TextInputLayout = findViewById(R.id.txtFldPassWord)
+        val btnAccess: Button = findViewById(R.id.btnAccess)
+        val btnRegister: Button = findViewById(R.id.btnRegister)
+
+        textInputUser.editText!!.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s!!.isNotEmpty()) {
+                    textInputUser.error = null
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+        textInputPassword.editText!!.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s!!.isNotEmpty()) {
+                    textInputPassword.error = null
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+        btnAccess.setOnClickListener {
+            if (textInputUser.editText?.text.isNullOrEmpty()) {
+                textInputUser.error = "Invalid User"
+            } else if (textInputPassword.editText?.text.isNullOrEmpty()) {
+                textInputPassword.error = "Incorrect Password"
+            } else {
+                val mySQLite = SQLiteManager(this)
+                mySQLite.setWritable()
+                val myCursor = mySQLite.getUserLogin(
+                    textInputUser.editText!!.text.toString(),
+                    textInputPassword.editText!!.text.toString()
+                )
+                if (myCursor.moveToNext()) {
+                    mySQLite.getDb().close()
+                    textInputUser.editText!!.text.clear()
+                    textInputPassword.editText!!.text.clear()
+                    startActivityForResult(Intent(this@MainActivity, RecyclerviewActivity::class.java), RECYCLER_REQUEST)
+                } else {
+                    Snackbar.make(it, "yayayayayyai", Snackbar.LENGTH_LONG).show()
+                }
+            }
+        }
+
+        btnRegister.setOnClickListener {
+            startActivityForResult(Intent(this@MainActivity, RegisterActivity::class.java), REGISTER_REQUEST)
+        }
     }
 }
---
+++ app/src/main/java/com/politecnicomalaga/myktcrud/SQLiteManager.kt
@@ -0,0 +1,106 @@
+package com.politecnicomalaga.myktcrud
+
+import android.annotation.SuppressLint
+import android.content.ContentValues
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+const val DATABASE_NAME = "DBUsers.db"
+const val DATABASE_VERSION = 1
+
+@SuppressLint("StaticFieldLeak")
+private lateinit var myContext: Context
+private lateinit var db: SQLiteDatabase
+
+const val DB_TUsers = "TUsers"
+val TUsers_USER = arrayOf("cUser", "0")
+val TUsers_PASSWORD = arrayOf("cPassword", "1")
+val TUsers_BIRTHDATE = arrayOf("dBirthdate", "2")
+val TUsers_IMGPROFILE = arrayOf("bImgProfile", "3")
+val TUsers_ROLE = arrayOf("cRole", "4")
+
+const val DB_TUsersGroups = "TUsersGroups"
+val TUsersGroups_ROLE = arrayOf("cRole", "0")
+val TUG_ROLES = arrayOf("Admin", "Estandar", "Invitado")
+
+class SQLiteManager(context: Context) :
+    SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+    init {
+        myContext = context
+    }
+
+    override fun onCreate(db: SQLiteDatabase) {
+        db.execSQL(
+            "CREATE TABLE " + DB_TUsers + " ( " +
+                    TUsers_USER[0] + " VARCHAR(30) PRIMARY KEY," +
+                    TUsers_PASSWORD[0] + " VARCHAR(30) NOT NULL, " +
+                    TUsers_BIRTHDATE[0] + " VARCHAR(10) NOT NULL, " +
+                    TUsers_IMGPROFILE[0] + " BLOB, " +
+                    TUsers_ROLE[0] + " VRCHAR(10) NOT NULL) "
+        )
+
+        db.execSQL(
+            "CREATE TABLE " + DB_TUsersGroups + " ( " + TUsersGroups_ROLE[0] + " VARCHAR(10) PRIMARY KEY)"
+        )
+
+        for (st in TUG_ROLES) {
+            db.execSQL("INSERT INTO " + DB_TUsersGroups + " ( " + TUsersGroups_ROLE[0] + " ) VALUES ( '" + st + "' ) ")
+//            db.execSQL("INSERT INTO " + DB_TUsersGroups + " VALUES ( '" + st + "' ) ");
+        }
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        TODO("Not yet implemented")
+    }
+
+    fun setWritable() {
+        db = this.writableDatabase
+    }
+
+    fun getUserLogin(user: String, password: String): Cursor {
+        return db.query(
+            DB_TUsers,
+            null,
+            TUsers_USER[0] + " = ? AND " + TUsers_PASSWORD[0] + " = ?",
+            arrayOf(user, password),
+            null,
+            null,
+            null
+        )
+    }
+
+    fun getOneUser(user: String): Cursor {
+        return db.query(DB_TUsers, null, TUsers_USER[0] + " = ?", arrayOf(user), null, null, null)
+    }
+
+    fun getUsers(): Cursor {
+        return db.query(DB_TUsers, null, null, null, null, null, TUsers_USER[0])
+    }
+
+    fun getGroups(): Cursor {
+        return db.query(DB_TUsersGroups, null, null, null, null, null, TUsersGroups_ROLE[0])
+    }
+
+    fun insertUser(values: ContentValues) {
+        db.insert(DB_TUsers, null, values)
+    }
+
+    fun updateUser(values: ContentValues, user: String) {
+        db.update(
+            DB_TUsers, values, TUsers_USER[0] + " = ?", arrayOf(user)
+        )
+    }
+
+    fun deleteUser(user: String) {
+        db.delete(
+            DB_TUsers, TUsers_USER[0] + " = ?", arrayOf(user)
+        )
+    }
+
+    fun getDb(): SQLiteDatabase {
+        return db
+    }
+
+}
